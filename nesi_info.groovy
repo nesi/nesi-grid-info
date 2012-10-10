d0dc012e12f07f28d2cf9af0ef392e114f51bec1
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-10-11 09:28:56 +1300

    Added nesi00028 group

--- nesi_info.groovy
+++ nesi_info.groovy
@@ -22,6 +22,7 @@ nz = new VO(
 		hostDN = '/C=NZ/O=BeSTGRID/OU=The University of Auckland/CN=voms.bestgrid.org'
 		)
 
+
 // groups
 bestgrid = new Group(
 		vo = nz,
@@ -62,6 +63,11 @@ landcare_osgeo = new Group(
 nesi = new Group(
 		vo = nz,
 		fqan = "/nz/nesi"
+		)
+
+nesi00028 = new Group(
+		vo = nz,
+		fqan = "/nz/nesi/projects/nesi00028"
 		)
 
 test = new Group(
@@ -238,6 +244,7 @@ auckland_cluster_groups = [
 	demo,
 	grid_dev,
 	nesi,
+	nesi00028,
 	uoa,
 	uoa_comp_chem,
 	uoa_gaussian,
@@ -867,7 +874,7 @@ scenz_packages = [
 pan_pan = new Queue(
 		gateway:pan,
 		name:'pan',
-		groups:[bestgrid, nesi],
+		groups:[bestgrid, nesi, nesi00028],
 		directories:[auckland_pan],
 		packages:pan_default_packages,
 		description:'Suitable for any jobs by NeSI members',
@@ -882,7 +889,7 @@ pan_pan = new Queue(
 pan_gpu = new Queue(
 		gateway:pan,
 		name:'gpu',
-		groups:[bestgrid, nesi],
+		groups:[bestgrid, nesi, nesi00028],
 		directories:[auckland_pan],
 		packages:pan_default_packages,
 		description:'GPU nodes on the Pan cluster',
